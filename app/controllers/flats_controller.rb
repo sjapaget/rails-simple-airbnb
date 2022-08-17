1521d5e56f630b000d96ea78036c56c959d30205
Samuel Paget <sjapaget@gmail.com>
2022-08-17 16:07:41 +0200

    Add flats#new and flats#create methods + view with form to submit new flats to db

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -6,9 +6,27 @@ class FlatsController < ApplicationController
     @flats = Flat.all
   end
 
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show; end
 
   private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 
   def set_flat
     @flat = Flat.find(params[:id])
