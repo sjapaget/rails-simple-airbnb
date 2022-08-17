b9c60ef38920f5fcfc64c859744d57e04cb5bba1
Samuel Paget <sjapaget@gmail.com>
2022-08-17 17:02:42 +0200

    Add destroy functionality for flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,6 @@
 class FlatsController < ApplicationController
 
-  before_action :set_flat, only: [:show, :edit, :update]
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
 
   def index
     @flats = Flat.all
@@ -28,6 +28,11 @@ class FlatsController < ApplicationController
     @flat.update(flat_params)
 
     redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
   end
 
   private
